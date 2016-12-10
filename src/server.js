91952554c1da04952e25ec2916e600d3f7eda20a
Raphael Benitte <benitte@ekino.com>
2016-12-10 09:11:01 +0900

    feat(template): adjust server.js

--- src/server.js
+++ src/server.js
@@ -1,28 +1,28 @@
-import express from 'express';
-import Mozaik  from 'mozaik';
-import config  from '../config';
-import travis  from 'mozaik-ext-travis/client';
+import express from 'express'
+import Mozaik  from 'mozaik'
+import config  from '../config'
+//import EXT_NAME  from 'mozaik-ext-EXT_NAME/client'
 
 
 const forceSSL = (req, res, next) => {
     if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(['https://', req.get('Host'), req.url].join(''));
+        return res.redirect(['https://', req.get('Host'), req.url].join(''))
     }
 
-    return next();
-};
+    return next()
+}
 
 
 const mozaik = new Mozaik(config);
-mozaik.bus.registerApi('travis', travis);
+//mozaik.bus.registerApi('EXT_NAME', EXT_NAME);
 
 
-const app    = express();
-const useSSL = process.env.USE_SSL === 'true';
+const app    = express()
+const useSSL = process.env.USE_SSL === 'true'
 if (useSSL) {
-    app.use(forceSSL);
+    app.use(forceSSL)
 }
-mozaik.startServer(app);
+mozaik.startServer(app)
 
 
-export default mozaik;
+export default mozaik
